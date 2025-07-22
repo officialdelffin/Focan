7ab9b6c8cbc317a6eaf3b93f393e78ff1211c84a
officialdelffin <workdelffin@gmail.com>
2025-07-21 21:49:08 -0300

    Commit 2

--- app/src/main/java/com/example/focan/TimerActivity.kt
+++ app/src/main/java/com/example/focan/TimerActivity.kt
@@ -12,41 +12,40 @@ import androidx.core.view.WindowInsetsCompat
 
 class TimerActivity : AppCompatActivity() {
 
-    lateinit var textCounter: TextView
-    lateinit var dotOne: ImageView
-    lateinit var dotTwo: ImageView
-    lateinit var dotThree: ImageView
-    lateinit var buttonStart: AppCompatButton
-    lateinit var buttonPause: AppCompatButton
-    lateinit var buttonResete: AppCompatButton
+    lateinit var textCounter  : TextView
+    lateinit var dotOne       : ImageView
+    lateinit var dotTwo       : ImageView
+    lateinit var dotThree     : ImageView
+    lateinit var buttonStart  : AppCompatButton
+    lateinit var buttonPause  : AppCompatButton
+    lateinit var buttonResete : AppCompatButton
 
     private var totalTimerMilliseconds = 25 * 60 * 1000L
     private var counter: CountDownTimer? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate( savedInstanceState: Bundle? ) {
 
-        super.onCreate(savedInstanceState)
+        super.onCreate( savedInstanceState )
         enableEdgeToEdge()
-        setContentView(R.layout.activity_timer)
+        setContentView( R.layout.activity_timer )
         startComponents()
-
         buttonStart.setOnClickListener { funcionCounter() }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener( findViewById( R.id.main ) ) { v, insets ->
+            val systemBars = insets.getInsets( WindowInsetsCompat.Type.systemBars() )
+            v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom )
             insets
         }
     }
 
     private fun startComponents() {
 
-        textCounter = findViewById(R.id.textCounter)
-        dotOne = findViewById(R.id.dotOne)
-        dotTwo = findViewById(R.id.dotTwo)
-        dotThree = findViewById(R.id.dotThree)
-        buttonStart = findViewById(R.id.buttonStart)
-        buttonPause = findViewById(R.id.buttonPause)
+        textCounter  = findViewById(R.id.textCounter)
+        dotOne       = findViewById(R.id.dotOne)
+        dotTwo       = findViewById(R.id.dotTwo)
+        dotThree     = findViewById(R.id.dotThree)
+        buttonStart  = findViewById(R.id.buttonStart)
+        buttonPause  = findViewById(R.id.buttonPause)
         buttonResete = findViewById(R.id.buttonResete)
 
     }
@@ -55,14 +54,14 @@ class TimerActivity : AppCompatActivity() {
 
         counter?.cancel()
 
-        counter = object : CountDownTimer(totalTimerMilliseconds, 1000) {
+        counter = object : CountDownTimer( totalTimerMilliseconds, 1000 ) {
 
-            override fun onTick(millisUntilFinished: Long) {
+            override fun onTick( millisUntilFinished: Long ) {
 
-                val minutes = (millisUntilFinished / 1000) / 60
-                val seconds = (millisUntilFinished / 1000) % 60
+                val minutes = ( millisUntilFinished / 1000 ) / 60
+                val seconds = ( millisUntilFinished / 1000 ) % 60
 
-                val formattedTime = String.format("%02d:%02d", minutes, seconds)
+                val formattedTime = String.format( "%02d:%02d" , minutes , seconds )
 
                 textCounter.text = formattedTime
 
@@ -77,7 +76,6 @@ class TimerActivity : AppCompatActivity() {
         }
 
         counter?.start()
-
 
     }
 }
