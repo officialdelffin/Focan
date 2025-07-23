f8caeeb57239aaeceb2483a76565dd24fd5fbfdc
officialdelffin <workdelffin@gmail.com>
2025-07-23 17:03:47 -0300

    Commit 5

--- app/src/main/java/com/example/focan/TimerActivity.kt
+++ app/src/main/java/com/example/focan/TimerActivity.kt
@@ -22,10 +22,15 @@ class TimerActivity : AppCompatActivity() {
     lateinit var buttonPause  : AppCompatButton
     lateinit var buttonResete : AppCompatButton
 
-    private var totalTimerMilliseconds = 25 * 60 * 1000L  // 25 minutes em milliseconds
-    private var counter: CountDownTimer? = null           // Creating a stopwatch
-    private var timeLeftMillis = totalTimerMilliseconds   // Amarzening the time left
-    private var isRunning = false                         // The stopwatch is not running
+    private var totalTimerMillisecondsStudy = 2 * 60 * 1000L               // 25 minutes in milliseconds
+    private var totalTimerMillisecondsInterval = 1 * 60 * 1000L             // 5 minutes in milliseconds
+    private var counterStudy: CountDownTimer? = null                        // Creating a stopwatch for study
+    private var counterInterval: CountDownTimer ? = null                    // Creating a stopwatch for interval
+    private var timeLeftMillisStudy = totalTimerMillisecondsStudy           // Amarzening the time left
+    private var timeLeftMillisInterval = totalTimerMillisecondsInterval     // Amarzening the time left
+    private var isStudyTime = true                                          // The stopwatch is in study mode
+    private var isRunning = false                                           // The stopwatch is not running
+
 
     override fun onCreate( savedInstanceState: Bundle? ) {
 
@@ -35,9 +40,9 @@ class TimerActivity : AppCompatActivity() {
         startComponents()
 
         // Defining button clicks :
-        buttonStart.setOnClickListener { if ( !isRunning ) { funcionCounter() } }
-        buttonPause.setOnClickListener { funcionPause() }
-        buttonResete.setOnClickListener { funcionResete() }
+        buttonStart.setOnClickListener  { if ( !isRunning ) { funcionCheckRunning() } }
+        buttonPause.setOnClickListener  { funcionPause() }
+        buttonResete.setOnClickListener { checkResete() }
 
         ViewCompat.setOnApplyWindowInsetsListener( findViewById( R.id.main ) ) { v, insets ->
             val systemBars = insets.getInsets( WindowInsetsCompat.Type.systemBars() )
@@ -59,18 +64,47 @@ class TimerActivity : AppCompatActivity() {
 
     }
 
-    private fun funcionCounter() {
+    private fun funcionCheckRunning () {
+
+        if ( !isRunning ) {
+
+            if ( isStudyTime ) {
+
+                funcionCounterStudy()
+
+            }
+
+            else {
+
+                funcionCounterInterval()
+
+            }
+
+            isRunning = true
+
+        }
+
+    }
+
+    private fun checkResete() {
+
+        if ( isStudyTime ) { funcionReseteStudy() }
+        else { funcionReseteInterval() }
+
+    }
+
+    private fun funcionCounterStudy() {
 
         // Canceling if the stopwatch is running :
-        counter?.cancel()
+        counterStudy?.cancel()
 
         // Creating the object counter and defining the time and the time interval :
-        counter = object : CountDownTimer( timeLeftMillis , 1000 ) {
+        counterStudy = object : CountDownTimer( timeLeftMillisStudy , 1000 ) {
 
             // Updating every second :
             override fun onTick( millisUntilFinished : Long ) {
 
-                timeLeftMillis = millisUntilFinished
+                timeLeftMillisStudy = millisUntilFinished
 
                 val minutes = ( millisUntilFinished / 1000 ) / 60
                 val seconds = ( millisUntilFinished / 1000 ) % 60
@@ -84,34 +118,86 @@ class TimerActivity : AppCompatActivity() {
             override fun onFinish() {
 
                 textCounter.text = "00:00"
+                isStudyTime = false
+                isRunning = false
+                funcionCounterInterval()
 
             }
 
         }
 
-        counter?.start() // Starting the stopwatch
+        counterStudy?.start() // Starting the stopwatch
+
+    }
+
+    private fun funcionCounterInterval() {
+
+        counterInterval?.cancel()
+
+        counterInterval = object : CountDownTimer( totalTimerMillisecondsInterval , 1000 ) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+                timeLeftMillisInterval = millisUntilFinished
+
+                var minutes = (millisUntilFinished / 1000) / 60
+                var seconds = (millisUntilFinished / 1000) % 60
+
+                val formattedTime = String.format("%02d:%02d", minutes, seconds)
+
+                textCounter.text = formattedTime
+
+            }
+
+            override fun onFinish() {
+
+                textCounter.text = "00:00"
+                isStudyTime = true
+                isRunning = false
+                funcionCounterStudy()
+
+            }
+
+        }
+
+        counterInterval?.start()
 
     }
 
     private fun funcionPause() {
 
-        counter?.cancel()  // Canceling the stopwatch
-        isRunning = false  // The stopwatch is not running
+        counterStudy?.cancel()     // Canceling the stopwatch
+        counterInterval?.cancel()  // Canceling the stopwatch
+        isRunning = false          // The stopwatch is not running
 
     }
 
-    private fun funcionResete() {
+    private fun funcionReseteStudy() {
 
-        counter?.cancel()                         // Canceling the stopwatch
-        timeLeftMillis = totalTimerMilliseconds   // Resetting the time
-        isRunning = false                         // The stopwatch is not running
+        counterStudy?.cancel()                              // Canceling the stopwatch
+        timeLeftMillisStudy = totalTimerMillisecondsStudy   // Resetting the time
+        isRunning = false                                   // The stopwatch is not running
 
-        val minutes = (timeLeftMillis / 1000) / 60
-        val seconds = (timeLeftMillis / 1000) % 60
+        val minutes = (timeLeftMillisStudy / 1000) / 60
+        val seconds = (timeLeftMillisStudy / 1000) % 60
         val formattedTime = String.format("%02d:%02d", minutes, seconds)
 
         textCounter.text = formattedTime
 
+    }
+
+    private fun funcionReseteInterval() {
+
+        counterInterval?.cancel()
+        timeLeftMillisInterval = totalTimerMillisecondsInterval
+        isRunning = false
+
+        val minutes = ( timeLeftMillisInterval / 1000 ) / 60
+        val seconds = ( timeLeftMillisInterval / 1000 ) % 60
+
+        val formattedTime = String.format( "%02d:%02d" , minutes , seconds )
+
+        textCounter.text = formattedTime
 
     }
 
